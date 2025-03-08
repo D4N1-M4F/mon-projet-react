beb39b10ff4ccd0c2150d64932da0c3e178ee19e
D4N1-M4F <85316076+D4N1-M4F@users.noreply.github.com>
2025-03-08 17:56:29 -0500

    Initialisation du projet React avec Ant Design

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenue sur mon projet React + Ant Design</h1>
+      <Button type="primary">Clique-moi !</Button>
+    </div>
+  );
+}
+
+export default App;
